6d5016ce15b5c8076f9983393e651d9379b04b84
Mateo Enriquez <laboratorio@udla-ec.int>
2023-11-21 15:13:02 -0500

    Demas for agregados para asignar el valor 1 a la diagonal principal

--- diagonal_en_unos.c
+++ diagonal_en_unos.c
@@ -10,15 +10,39 @@ int main(){
     scanf("%d", &m);
 
     int matrix[n][m];
+    int suma;
 
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < m; j++)
         {
             matrix[i][j]=0;
-        }
-        
+        }   
     }
+    
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            
+            
+            if (i==j)
+            {
+                matrix[i][j]=1;
+            }
+            
+        }   
+    }
+
+        for (int i = 0; i < n; i++)
+    {
+        printf("\n");
+        for (int j = 0; j < m; j++)
+        {
+            printf("%d", matrix[i][j]);
+        }   
+    }
+
         
 
     return 0;
