ac219e53a906c52a7aff59e42130383b0530fa8e
Mateo Enriquez <laboratorio@udla-ec.int>
2023-11-21 15:03:25 -0500

    Matrix inicializada

--- diagonal_en_unos.c
+++ diagonal_en_unos.c
@@ -7,7 +7,19 @@ int main(){
     printf("Ingrese el numero de filas: ");
     scanf("%d", &n);
     printf("Ingrese el numero de columnas: ");
-    scanf("%d", &m);    
+    scanf("%d", &m);
+
+    int matrix[n][m];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            matrix[i][j]=0;
+        }
+        
+    }
+        
 
     return 0;
 }
