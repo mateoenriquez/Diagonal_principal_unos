813d41557a53e34bab11d7a57dc7f8dbf7b5c026
Laboratorio <laboratorio@udla-ec.int>
2023-11-21 14:57:50 -0500

    Variables inicializadas y el ingreso de datos

---
+++ diagonal_en_unos.c
@@ -0,0 +1,13 @@
+#include "stdio.h"
+
+int main(){
+
+    int n, m;
+    
+    printf("Ingrese el numero de filas: ");
+    scanf("%d", &n);
+    printf("Ingrese el numero de columnas: ");
+    scanf("%d", &m);    
+
+    return 0;
+}
